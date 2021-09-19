56dfa0867a9b72023aa61756698a20b6afb7afe6
Skrukow245 <skrukow245@gmail.com>
2021-09-18 20:27:08 -0600

    added comments

--- Boolean Logic Assignment/Program.cs
+++ Boolean Logic Assignment/Program.cs
@@ -9,15 +9,15 @@ namespace Boolean_Logic_Assignment
         {
             Console.WriteLine("Car Insurance Application");
             Console.WriteLine("What is your age");
-            string age = Console.ReadLine();
-            int age1 = Int32.Parse(age);
+            string age = Console.ReadLine();               
+            int age1 = Int32.Parse(age);        //turning string into int
             Console.WriteLine("Never had a DUI before? True or False");
             string DUI = Console.ReadLine();
-            bool noDUI = bool.Parse(DUI);
+            bool noDUI = bool.Parse(DUI);   //turning string into bool
             Console.WriteLine("How many speeding tickets do you have?");
             string tickets = Console.ReadLine();
             int tickets1 = Int32.Parse(tickets);
-            bool Qualified = (age1 > 15 && noDUI && tickets1 <= 3);
+            bool Qualified = (age1 > 15 && noDUI && tickets1 <= 3);  //using && to see if bool is correct
             Console.WriteLine("Are you qualified?");
             Console.WriteLine(Qualified);
             Console.ReadLine();
